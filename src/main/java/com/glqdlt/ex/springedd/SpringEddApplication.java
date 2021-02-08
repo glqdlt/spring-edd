2b35011211b172b9a62ab198a8ee0add10e5bc57
glqdlt <dlfdnd0725@gmail.com>
2021-02-08 12:47:23 +0900

    Init

---
+++ src/main/java/com/glqdlt/ex/springedd/SpringEddApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.springedd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glqdlt/ex/springedd/SpringEddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.springedd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
